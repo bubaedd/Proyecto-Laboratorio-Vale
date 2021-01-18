ac4007dad3484f28a98447ae4f48158b8969356f
BenjaminVargasXLR <benjamin.vargas@mirsrobotics.com>
2021-01-17 21:43:20 -0300

    Programa con Avance de graficos

--- Graficos.py
+++ Graficos.py
@@ -1,4 +1,5 @@
 import os.path as path
+from matplotlib import  pyplot
 import os
 
 def graficos():
@@ -12,4 +13,32 @@ def graficos():
     if os.stat("respuestas.txt").st_size == 0:
         print("El archivo está vacío, por favor completa la enuesta para poder ver los gráficos")
         return 0
+
+    else:
+        contLine =0
+        countSI =0
+        countNO=0
+        with open("respuestas.txt") as lines:
+            for line in lines:
+                line.replace("\n","")
+                value = line.split(';')
+
+
+                for i in range(len(value)):
+
+                    if value[i]=="SI" or value[i]=="SI\n":
+                        countSI+=1
+
+                    if value[i]=="NO" or value[i]=="NO\n":
+                        countNO+=1
+        resultadosEncuesta = ["SI","NO"]
+        nSINO=[countSI,countNO]
+        colores=["green","red"]
+
+        pyplot.title("resultados de encuestas")
+        pyplot.bar(resultadosEncuesta,height=nSINO,color=colores,width=0.5)
+
+        pyplot.show()
+
+
 
